7a159f1839c1de77540314b39bc434677098f70a
Tabernicola <tabernicola01@gmail.com>
2014-11-29 12:29:01 +0100

    first commit

---
+++ Entity/User.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tabernicola\JukeCloudUserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Entity\User as FosUser;
+/**
+ * User
+ */
+class User extends FosUser
+{
+    
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+User bundle for JukeCloud
